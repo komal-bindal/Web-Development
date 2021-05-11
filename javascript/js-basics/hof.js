2675b5ed22af22ca311d1e76ab6c79a17ce39783
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-05-11 23:12:39 +0530

    create an higher order function.

--- javascript/js-basics/hof.js
+++ javascript/js-basics/hof.js
@@ -2,3 +2,20 @@
  * higher order function--
  * pass a function to another function or return a function from another function
  */
+function createGreeter(greeting){
+    function greet(name){
+        console.log(greeting,name)
+    }
+    return greet
+    //in case of return greet(), datatype of g1 will be undefined.
+}
+let g1 = createGreeter("Good morning")
+let g2 = createGreeter("Good evening")
+
+console.log(typeof g1) //function
+console.log(g1('Komal'))
+console.log(g1('Dev'))
+console.log(g2('Komal'))
+console.log(g2('Dev'))
+
+ 
